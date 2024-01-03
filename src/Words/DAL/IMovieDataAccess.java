d8c6e88c01bdaf2fb3beeb4c9bb72d5345bfe19a
OliSparrow <jay_jakobsen@outlook.dk>
2024-01-03 11:22:48 +0100

    CRUD in interface

--- src/Words/DAL/IMovieDataAccess.java
+++ src/Words/DAL/IMovieDataAccess.java
@@ -1,4 +1,18 @@
 package Words.DAL;
 
+import Words.BE.Movie;
+
+import java.util.List;
+
 public interface IMovieDataAccess {
+
+    public List<Movie> getAllMovies() throws Exception;
+    
+    public Movie createMovie(Movie movie) throws Exception;
+
+    public void updateMovie(Movie movie) throws Exception;
+
+    public void deleteMovie(Movie movie) throws Exception;
+
+
 }
