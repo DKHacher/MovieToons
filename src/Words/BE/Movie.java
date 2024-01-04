c7ec4e1d75d060848e004d65f8d14121bae120d0
OliSparrow <jay_jakobsen@outlook.dk>
2024-01-04 10:57:59 +0100

    Start of making MovieDAO + Movie Class

--- src/Words/BE/Movie.java
+++ src/Words/BE/Movie.java
@@ -1,4 +1,49 @@
 package Words.BE;
 
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaException;
+
+import java.io.File;
+
 public class Movie {
+
+    private String fileLink, movieTitle, lastView;
+    private int id, ratingIMDB, ratingPersonal;
+    private Media media;
+
+    public Movie(int id, String movieTitle, int ratingIMDB, int ratingPersonal, String fileLink, String lastView) {
+        this.id = id;
+        this.fileLink = fileLink;
+        this.movieTitle = movieTitle;
+        this.ratingIMDB = ratingIMDB;
+        this.ratingPersonal = ratingPersonal;
+        this.lastView = lastView;
+
+        try {
+            this.media = new Media(new File(fileLink).toURI().toString());
+        } catch (MediaException e) {
+            System.out.println("Error creating Media object " + fileLink);
+            e.printStackTrace();
+        }
+    }
+
+    public String getMovieTitle() {
+        return movieTitle;
+    }
+
+    public int getIMDB() {
+        return ratingIMDB;
+    }
+
+    public int getPersonal() {
+        return ratingPersonal;
+    }
+
+    public String getFilePath() {
+        return fileLink;
+    }
+
+    public  getLastView() {
+        return lastView;
+    }
 }
--- src/Words/DAL/db/MovieDAO_DB.java
+++ src/Words/DAL/db/MovieDAO_DB.java
@@ -1,4 +1,93 @@
 package Words.DAL.db;
 
-public class MovieDAO_DB {
+import Words.BE.Movie;
+import Words.DAL.IMovieDataAccess;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovieDAO_DB implements IMovieDataAccess {
+
+    private DatabaseConnector databaseConnector;
+
+    public MovieDAO_DB() throws Exception {
+        databaseConnector = new DatabaseConnector();
+    }
+
+    @Override
+    public List<Movie> getAllMovies() throws Exception{
+        ArrayList<Movie> allMovies = new ArrayList<>();
+
+        try (Connection conn = databaseConnector.getConnection();
+             Statement stmt = conn.createStatement())
+        {
+            String sql =
+                    """
+                    SELECT Movies.id, Movies.name, Movies.ratingIMDB, Movies.ratingPersonal, fileLink, lastView
+                    FROM Movies
+                    """;
+
+            ResultSet rs = stmt.executeQuery(sql);
+            // Loop through rows from the database result set
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String movieTitle = rs.getString("name");
+                int ratingIMDB = rs.getInt("ratingIMDB");
+                int ratingPersonal = rs.getInt("ratingPersonal");
+                String fileLink = rs.getString("fileLink");
+                String lastView = rs.getString("lastView");
+
+                Movie movie = new Movie(id, movieTitle, ratingIMDB, ratingPersonal, fileLink, lastView);
+                allMovies.add(movie);
+            }}
+        return allMovies;
+    }
+
+    @Override
+    public Movie createMovie(Movie movie) throws Exception {
+        // SQL command
+        String sql = "INSERT INTO Movies (name, ratingIMDB, ratingPersonal, fileLink, lastView) VALUES (?, ?, ?, ?);";
+
+        try (Connection conn = databaseConnector.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            // Bind parameters
+            stmt.setString(1, movie.getMovieTitle());
+            stmt.setLong(2, movie.getIMDB());
+            stmt.setLong(3, movie.getPersonal());
+            stmt.setLong(4, movie.getFilePath());
+            stmt.setLong(5, movie.getLastView());
+
+            // Run the specified SQL statement
+            stmt.executeUpdate();
+
+            // Get the generated ID from the DB
+            ResultSet rs = stmt.getGeneratedKeys();
+
+            int id = 0;
+            if (rs.next()) {
+                id = rs.getInt(1);
+            }
+
+            Movie createdMovie = new Movie(id, movie.getMovieTitle(), movie.getIMDB(), movie.getPersonal(), movie.getFilePath(), movie.getLastView());
+
+            return createdMovie;
+        } catch (SQLException ex) {
+            // create entry in log file
+            ex.printStackTrace();
+            throw new Exception("Could not insert movie.", ex);
+        }
+    }
+
+    @Override
+    public void updateMovie(Movie movie) throws Exception {
+
+    }
+
+    @Override
+    public void deleteMovie(Movie movie) throws Exception {
+
+    }
+
 }
